c65958d4031aa0e5fe1e2cc190f5c9a51ef044c7
Mohamed Fayis Elayodath <mfayize@gmail.com>
2023-12-12 14:40:52 +0530

    fix caching issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -38,6 +38,7 @@ const IssueForm = ({issue}:Props) => {
             else
                 await axios.post('/api/issues', data)
             router.push('/issues')
+            router.refresh()
         } catch (error) {
             setError('An unexpected error occured.')
             setSubmitting(false)
