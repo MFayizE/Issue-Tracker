a68ac9b317f0a07c6357626e10c7b5a5883da7e2
Mohamed Fayis Elayodath <mfayize@gmail.com>
2023-12-12 14:03:23 +0530

    add the Edit Button

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,6 @@
+import { Skeleton } from '@/app/components'
 import { Flex, Card, Box } from '@radix-ui/themes'
 import React from 'react'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
 const LoadingIssueDetailPage = () => {
     return (
         <Box className='max-w-xl'>
