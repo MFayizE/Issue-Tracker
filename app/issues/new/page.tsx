8916e561f27d6c0710d714e1e311e7d0ca94472e
Mohamed Fayis Elayodath <mfayize@gmail.com>
2023-12-12 12:57:40 +0530

    disabled ssr for loading SimpleMDE editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { Button, Callout, Text, TextField } from '@radix-ui/themes'
-import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
@@ -11,6 +10,12 @@ import { z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
 import Spinner from '@/app/components/Spinner';
 import { useState } from 'react';
+import dynamic from 'next/dynamic';
+
+const SimpleMDE = dynamic(
+    () => import('react-simplemde-editor'), 
+    {ssr:false})
+
 type IssueForm = z.infer<typeof createIssueSchema>;
 
 
@@ -51,7 +56,8 @@ const NewIssuePage = () => {
                 <ErrorMessage >{errors.title?.message}</ErrorMessage>
                 <Controller name='description'
                     control={control}
-                    render={({ field }) => <SimpleMDE placeholder='Description' {...field} />
+                    render={({ field }) => 
+                    <SimpleMDE placeholder='Description' {...field} />
                     } />
                 <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
