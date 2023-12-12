3e44aa9ad927e1409a9e38fffdba3b5ad95ec296
Mohamed Fayis Elayodath <mfayize@gmail.com>
2023-12-12 19:54:27 +0530

    sort Issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -24,12 +24,13 @@ const IssuesPage = async ({ searchParams }: Props) => {
 
     const statusList = Object.values(Status)
     const status = statusList.includes(searchParams.status) ? searchParams.status : undefined
-
+    const orderBy = columns.map(column => column.value).includes(searchParams.orderBy) ? {[searchParams.orderBy]: 'asc'} : undefined
 
     const issues = await prisma.issue.findMany({
         where: {
             status
-        }
+        },
+        orderBy
     })
     return (
         <div>
