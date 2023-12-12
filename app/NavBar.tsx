fc270bbe5b3327b45e88bd6bf717a8d8cbde8cb8
Mohamed Fayis Elayodath <mfayize@gmail.com>
2023-12-12 17:04:22 +0530

    add a dropdown menu to show current user

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import { IoBug } from "react-icons/io5";
 import classnames from 'classnames';
 import { useSession } from 'next-auth/react';
-import { Box, Container, Flex } from '@radix-ui/themes';
+import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
 
 const NavBar = () => {
     const currentPath = usePathname()
@@ -36,9 +36,27 @@ const NavBar = () => {
                     </Flex>
                     <Box>
                         {status === 'authenticated' &&
-                            <Link href='/api/auth/signout'>
-                                Log out
-                            </Link>
+                            <DropdownMenu.Root>
+                                <DropdownMenu.Trigger>
+                                    <Avatar src={session.user!.image!}
+                                        fallback="?"
+                                        size='2'
+                                        radius='full'
+                                        className='cursor-pointer'
+                                    />
+                                </DropdownMenu.Trigger>
+                                <DropdownMenu.Content>
+                                    <DropdownMenu.Label>
+                                        <Text size="2">{session.user!.email}</Text>
+                                    </DropdownMenu.Label>
+                                    <DropdownMenu.Item>
+                                        <Link href='/api/auth/signout'>
+                                            Log out
+                                        </Link>
+                                    </DropdownMenu.Item>
+                                </DropdownMenu.Content>
+                            </DropdownMenu.Root>
+
                         }
                         {status === 'unauthenticated' &&
                             <Link href='/api/auth/signin'>
